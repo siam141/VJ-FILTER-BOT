2bffb78d96e5ec79c294bbb7692595008be6372b
siam141 <115353753+siam141@users.noreply.github.com>
2025-04-27 21:31:20 +0600

    Update info.py

--- info.py
+++ info.py
@@ -15,6 +15,9 @@ API_ID = int(environ.get('API_ID', ''))
 API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
+TMDB_API_KEY = "c3443ed2f96cd615e3badf6b68c8a689"
+
+
 
 #For Request optional
 
