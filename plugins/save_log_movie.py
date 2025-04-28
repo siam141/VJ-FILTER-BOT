f1413f43490238494579db38e81e5a1942a853dd
siam141 <115353753+siam141@users.noreply.github.com>
2025-04-28 11:36:22 +0600

    Update save_log_movie.py

--- plugins/save_log_movie.py
+++ plugins/save_log_movie.py
@@ -1,9 +1,12 @@
 from pyrogram import Client, filters
 from database.todaymovies import save_today_movie
+from helper import get_clean_title
 
 LOG_CHANNEL_ID = -1002589776901
 
 @Client.on_message(filters.chat(LOG_CHANNEL_ID) & filters.video)
 async def save_movie_to_db(client, message):
-    title = message.caption if message.caption else "No Title"
+    title = message.caption
+    if not title:
+        title = get_clean_title(message.video.file_name)
     await save_today_movie(title)
