e95005b585eb47d65fea98b851bb2f4bfc37e893
siam141 <115353753+siam141@users.noreply.github.com>
2025-04-28 11:56:20 +0600

    Create movie_listeners.py

---
+++ plugins/movie_listeners.py
@@ -0,0 +1,11 @@
+# plugins/movie_listeners.py
+
+from pyrogram import Client, filters
+from database.today_movies_db import save_movie
+
+LOG_CHANNEL_ID = -1002589776901  # তোমার লগ চ্যানেল আইডি
+
+@Client.on_message(filters.chat(LOG_CHANNEL_ID) & filters.video)
+async def save_video_info(client, message):
+    title = message.caption or message.video.file_name or "Untitled"
+    await save_movie(title)
