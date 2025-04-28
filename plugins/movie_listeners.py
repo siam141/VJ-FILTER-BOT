c0430776263613d5eb93e07b12d4f864b9e7008c
siam141 <115353753+siam141@users.noreply.github.com>
2025-04-28 13:01:46 +0600

    Update movie_listeners.py

--- plugins/movie_listeners.py
+++ plugins/movie_listeners.py
@@ -3,7 +3,7 @@
 from pyrogram import Client, filters
 from database.today_movies_db import save_movie
 
-LOG_CHANNEL_ID = -1002589776901  # তোমার লগ চ্যানেল আইডি
+LOG_CHANNEL_ID = -1002589776901  # তোমার লগ চ্যানেলের ID
 
 @Client.on_message(filters.chat(LOG_CHANNEL_ID) & filters.video)
 async def save_video_info(client, message):
