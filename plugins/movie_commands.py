0900e57967dedf53c1682327d198355197ba69d3
siam141 <115353753+siam141@users.noreply.github.com>
2025-04-28 12:48:19 +0600

    Update movie_commands.py

--- plugins/movie_commands.py
+++ plugins/movie_commands.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 from database.today_movies_db import get_today_movies
 from datetime import datetime
 
-POST_CHANNEL_ID = -1001234567890  # এখানে তোমার পোস্ট করার চ্যানেলের আইডি বসাও
+POST_CHANNEL_ID = -1002589776901  # এখানে তোমার পোস্ট করার চ্যানেলের আইডি বসাও
 
 @Client.on_message(filters.command("listtoday"))
 async def list_today_movies(client, message):
